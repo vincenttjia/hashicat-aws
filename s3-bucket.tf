8aea17a430093b103bc4a706e3cdeaae09098ea1
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-10-13 03:58:05 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,5 @@ module "s3_bucket" {
   source = "app.terraform.io/vincent/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket-jagoejgoaisjoiasjhoes"
   bucket_prefix = var.prefix
-  acl    = "private"
-
 }
