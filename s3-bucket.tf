57b95aea472e9810cbfcd391258328388852e456
Vincent Tjianattan <vincent.tjianattan@binus.ac.id>
2021-10-13 03:57:01 +0000

    fix s3 module url

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
 module "s3_bucket" {
-  source = "vincent/s3-bucket/aws"
+  source = "app.terraform.io/vincent/s3-bucket/aws"
+  version = "2.2.0"
 
   bucket = "my-s3-bucket-jagoejgoaisjoiasjhoes"
   bucket_prefix = var.prefix
