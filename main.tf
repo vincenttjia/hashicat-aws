9ec5eeb5cb39a64d7e5ce31af8c62cc27f86fa11
Vincent Tjianattan <xvincent1000@gmail.com>
2021-10-15 14:09:48 +0700

    push

--- main.tf
+++ main.tf
@@ -133,7 +133,6 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = local.Department
-    Billable = "yes"
     Owner = "vincent"
   }
 }
